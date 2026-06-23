80ca22e5389a69fddef3bacc3475a5e446267f00
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 12:38:47 +0530

    optimization of main part 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from app.api.movies import router as movie_router
 
 app = FastAPI()
 
+app.include_router(movie_router)
+
 @app.get("/")
 def home():
-    return {
-        "message": "Hello Render"
-    }
+    return {"message": "OK"}
