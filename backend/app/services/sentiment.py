89a217996e357f7021973cd622a7ac1bf86de13d
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 13:20:04 +0530

    optimization of main part 4

--- backend/app/services/sentiment.py
+++ backend/app/services/sentiment.py
@@ -1,21 +1,3 @@
-from transformers import pipeline
-
-
-
-sentiment_model = None
-def get_sentiment_model():
-
-    global sentiment_model
-
-    if sentiment_model is None:
-
-        sentiment_model = pipeline(
-            "sentiment-analysis",
-            model="distilbert-base-uncased-finetuned-sst-2-english"
-        )
-
-    return sentiment_model
-
 def analyze_sentiment(text):
 
     return {
