59145035f17ec289e1f8ac9ef457373040761473
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 12:49:27 +0530

    optimization of main part 2

--- backend/app/api/movies.py
+++ backend/app/api/movies.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter
 import pandas as pd
 from pathlib import Path
+from huggingface_hub import hf_hub_download
+import joblib
+
 
 router = APIRouter()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-movies = pd.read_csv(
-    BASE_DIR / "data/processed/final_movies.csv"
+REPO_ID = "Samrc2255/movie-recommendation-models"
+movies_path = hf_hub_download(
+    repo_id=REPO_ID,
+    filename="movies.pkl"
 )
+
+movies = joblib.load(movies_path)
 
 @router.get("/movies/search/{query}")
 def search_movies(query: str):
