1f3d8b0f985aa0891dc7e624d2809916d041c17a
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 13:08:50 +0530

    optimization of main part 4

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.api.movies import router as movie_router
 from app.api.report import (router as report_router)
+from app.api.sentiment import (router as sentiment_router)
 
 app = FastAPI()
 
 app.include_router(movie_router)
 app.include_router(report_router)
+app.include_router(sentiment_router)
 
 @app.get("/")
 def home():
