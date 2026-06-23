370b1252a43565cc0b9f714b4c11bec21656c725
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 16:14:39 +0530

    optimization of recommend part 2

--- backend/app/api/recommend.py
+++ backend/app/api/recommend.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 from app.services.recommender import (
     recommend_with_scores
 )
-
+from app.services.hybrid import (
+    hybrid_recommend
+)
 router = APIRouter()
 
 @router.get("/recommend/{movie_name}")
@@ -19,3 +21,10 @@ def get_recommendations(
         "movie": movie_name,
         "recommendations": recommendations
     }
+@router.get("/hybrid-test")
+def hybrid_test():
+
+    return hybrid_recommend(
+        user_id=1,
+        movie_name="Avatar"
+    )
