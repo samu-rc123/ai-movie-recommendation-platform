ad9ba6ce3087d50d455a1b4cf06095aa5169d98b
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 14:23:04 +0530

    optimization of main part 5

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from app.api.movies import router as movie_router
 from app.api.report import (router as report_router)
 from app.api.sentiment import (router as sentiment_router)
+from app.api.recommend import router as recommend_router
 
 app = FastAPI()
 
 app.include_router(movie_router)
 app.include_router(report_router)
 app.include_router(sentiment_router)
+app.include_router(recommend_router)
 
 @app.get("/")
 def home():
-    return {"message": "OK"}
+    return {"message": "Movie Recommendation API Running"}
