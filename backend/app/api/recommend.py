532a249d258435471f61961f7bfbcaeaed94f996
Samrat Roy Choudhury <samratrc151@gmail.com>
2026-06-23 16:04:13 +0530

    optimization of recommend part 1

--- backend/app/api/recommend.py
+++ backend/app/api/recommend.py
@@ -1,9 +1,21 @@
 from fastapi import APIRouter
 
+from app.services.recommender import (
+    recommend_with_scores
+)
+
 router = APIRouter()
 
-@router.get("/recommend-test")
-def recommend_test():
+@router.get("/recommend/{movie_name}")
+def get_recommendations(
+    movie_name: str
+):
+
+    recommendations = recommend_with_scores(
+        movie_name
+    )
+
     return {
-        "status": "recommend router loaded"
+        "movie": movie_name,
+        "recommendations": recommendations
     }
